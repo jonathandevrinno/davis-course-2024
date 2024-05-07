3b176c6987364dc704c77aa4895aef7afbf3ea0d
jonathandevrinno <142668097+jonathandevrinno@users.noreply.github.com>
2024-05-07 21:00:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,6 @@ import streamlit as st
 import seaborn as sns
 
 data = pd.read_csv("tips.csv")
- 
-st.subheader("Jonathan Devrinno")
-st.subheader("21082010204")
 
 plt.scatter(data['day'], data['tip'])
 st.write("Scatter Plot")
@@ -18,3 +15,6 @@ st.write("Line Plot")
 fig, ax = plt.subplots(figsize=(10, 6))
 line_plot = sns.lineplot(data=data.drop(['total_bill'], axis=1), ax=ax)
 st.pyplot(fig)
+
+st.write(f'Nama : Jonathan Devrinno')
+st.write(f'NPM : 21082010204')
